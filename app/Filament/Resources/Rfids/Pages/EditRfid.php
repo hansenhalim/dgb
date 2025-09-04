51550ff89e9756ec6e2d19c4539f6c0c153cdde9
V Hansen Halim <fpsecond.hh@gmail.com>
2025-09-05 02:18:50 +0700

    wip

---
+++ app/Filament/Resources/Rfids/Pages/EditRfid.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Rfids\Pages;
+
+use App\Filament\Resources\Rfids\RfidResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRfid extends EditRecord
+{
+    protected static string $resource = RfidResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Rfids/RfidResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Resources\Rfids;
+
+use App\Filament\Resources\Rfids\Pages\CreateRfid;
+use App\Filament\Resources\Rfids\Pages\EditRfid;
+use App\Filament\Resources\Rfids\Pages\ListRfids;
+use App\Filament\Resources\Rfids\Schemas\RfidForm;
+use App\Filament\Resources\Rfids\Tables\RfidsTable;
+use App\Models\Rfid;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class RfidResource extends Resource
+{
+    protected static ?string $model = Rfid::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCreditCard;
+
+    public static function form(Schema $schema): Schema
+    {
+        return RfidForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return RfidsTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListRfids::route('/'),
+            'create' => CreateRfid::route('/create'),
+            'edit' => EditRfid::route('/{record}/edit'),
+        ];
+    }
+}
